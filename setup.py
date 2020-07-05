173eff46e2e876634eb5390064a3d424441fb574
Nv7 <junk4Nv7@gmail.com>
2020-07-05 15:21:29 -0700

    Added 2020.0.2

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    requirements = fh.read().split("\n")
+
+setup(
+    name="googlesearch-python",
+    version="2020.0.2",
+    author="Nishant Vikramaditya",
+    author_email="junk4Nv7@gmail.com",
+    description="A Python library for scraping the Google search engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nv7-GitHub/googlesearch",
+    packages=["googlesearch"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[requirements]
+)
