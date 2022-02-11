be88ef64de65529e5d42f14b4f2cd3f675ae4adf
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-02-10 22:10:17 -0800

    Support advanced search results

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -1,37 +1,63 @@
 from bs4 import BeautifulSoup
 from requests import get
 
+usr_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
-def search(term, num_results=10, lang="en", proxy=None):
-    usr_agent = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/61.0.3163.100 Safari/537.36'}
+def _req(term, results, lang, start, proxies):
+    resp = get(
+        url="https://www.google.com/search",
+        headers=usr_agent,
+        params=dict(
+            q = term,
+            num = results + 2, # Prevents multiple requests
+            hl = lang,
+            start = start,
+        ),
+        proxies=proxies,
+    )
+    resp.raise_for_status()
+    return resp
 
-    def fetch_results(search_term, number_results, language_code):
-        escaped_search_term = search_term.replace(' ', '+')
+class SearchResult:
+    def __init__(self, url, title, description):
+        self.url = url
+        self.title = title
+        self.description = description
 
-        google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results+1,
-                                                                              language_code)
-        proxies = None
-        if proxy:
-            if proxy[:5]=="https":
-                proxies = {"https":proxy} 
-            else:
-                proxies = {"http":proxy}
-        
-        response = get(google_url, headers=usr_agent, proxies=proxies)    
-        response.raise_for_status()
+    def __repr__(self):
+        return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
 
-        return response.text
+def search(term, num_results=10, lang="en", proxy=None, advanced=False):
+    escaped_term = term.replace(' ', '+')
 
-    def parse_results(raw_html):
-        soup = BeautifulSoup(raw_html, 'html.parser')
+    # Proxy
+    proxies = None
+    if proxy:
+        if proxy[:5]=="https":
+            proxies = {"https": proxy} 
+        else:
+            proxies = {"http": proxy}
+    
+    # Fetch
+    start = 0
+    while start < num_results:
+        # Send request
+        resp = _req(escaped_term, num_results-start, lang, start, proxies)
+
+        # Parse
+        soup = BeautifulSoup(resp.text, 'html.parser')
         result_block = soup.find_all('div', attrs={'class': 'g'})
         for result in result_block:
+            # Find link, title, description
             link = result.find('a', href=True)
             title = result.find('h3')
-            if link and title:
-                yield link['href']
+            description_box = result.find('div', {'style': '-webkit-line-clamp:2'})
+            if description_box:
+                description = description_box.find('span')
+                if link and title and description:
+                    start += 1
+                    if advanced:
+                        yield SearchResult(link['href'], title.text, description.text)
+                    else:
+                        yield link['href']
 
-    html = fetch_results(term, num_results, lang)
-    return list(parse_results(html))
