339c808a991d75468633a71f4e11c65df0090b0d
adanikel <59119926+adanikel@users.noreply.github.com>
2022-06-20 18:46:53 +0300

    use sleep

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from requests import get
+from time import sleep
 
 USER_AGENT = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
@@ -32,7 +33,7 @@ class SearchResult:
         return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
 
 
-def search(term, num_results=10, lang="en", proxy=None, advanced=False):
+def search(term, num_results=10, lang="en", proxy=None, advanced=False, sleep_interval=0):
     escaped_term = term.replace(" ", "+")
 
     # Proxy
@@ -65,3 +66,4 @@ def search(term, num_results=10, lang="en", proxy=None, advanced=False):
                         yield SearchResult(link["href"], title.text, description.text)
                     else:
                         yield link["href"]
+        sleep(sleep_interval)
