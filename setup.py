bc9eff9dd234348d1f2106a78dae712990e91aa7
Moritz Federspiel <moritz.federspiel@steadysense.at>
2023-04-03 20:48:08 +0200

    Add requirements directly to setup.py, bump to v1.2.1

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 with open("README.md", "r", encoding='UTF-8') as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding='UTF-8') as fh:
-    requirements = fh.read().split("\n")
+requirements=[
+    "beautifulsoup4>=4.9",
+    "requests>=2.20",
+]
 
 setup(
     name="googlesearch-python",
-    version="1.2.0",
+    version="1.2.1",
     author="Nishant Vikramaditya",
     author_email="junk4Nv7@gmail.com",
     description="A Python library for scraping the Google search engine.",
