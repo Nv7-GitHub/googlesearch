3061e4c7ea4d34bb1533940cc4ef1bd8b386512c
WY <hey.weiyang@gmail.com>
2023-04-20 21:20:48 -0400

    Update logic for no result

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -72,3 +72,7 @@ def search(term, num_results=10, lang="en", proxy=None, advanced=False, sleep_in
                     else:
                         yield link["href"]
         sleep(sleep_interval)
+
+        if start == 0:
+            print("No result found with this search term", escaped_term)
+            break
