80eb19690ee750a904dd3cbdfb77479000901beb
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2023-04-29 23:33:53 +0530

    Update __init__.py
    added typehint, replaced find with css selector, replaced generator with list

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -1,77 +1,86 @@
 """googlesearch is a Python library for searching Google, easily."""
 from time import sleep
 from bs4 import BeautifulSoup
-from requests import get
 from .user_agents import get_useragent
+from typing import List, Dict
+import requests
 
 
-def _req(term, results, lang, start, proxies, timeout):
-    resp = get(
-        url="https://www.google.com/search",
-        headers={
-            "User-Agent": get_useragent()
-        },
-        params={
-            "q": term,
-            "num": results + 2,  # Prevents multiple requests
-            "hl": lang,
-            "start": start,
-        },
-        proxies=proxies,
-        timeout=timeout,
-    )
-    resp.raise_for_status()
-    return resp
+def _req(
+    term: str,
+    results: int,
+    lang: str,
+    start: int,
+    proxies: Dict[str, str],
+    timeout: float,
+) -> requests.Response:
+    params = {"q": term, "num": results + 2, "hl": lang, "start": start}
+    headers = {"User-Agent": get_useragent()}
+    with requests.Session() as session:
+        session.headers.update(headers)
+        resp = session.get(
+            "https://www.google.com/search",
+            params=params,
+            proxies=proxies,
+            timeout=timeout,
+        )
+        resp.raise_for_status()
+        return resp
 
 
 class SearchResult:
-    def __init__(self, url, title, description):
+    def __init__(self, url: str, title: str, description: str) -> None:
         self.url = url
         self.title = title
         self.description = description
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
 
 
-def search(term, num_results=10, lang="en", proxy=None, advanced=False, sleep_interval=0, timeout=5):
-    """Search the Google search engine"""
-
+def search(
+    term: str,
+    num_results: int = 10,
+    lang: str = "en",
+    proxy: str = None,
+    advanced: bool = False,
+    sleep_interval: int = 0,
+    timeout: int = 5,
+) -> List:
     escaped_term = term.replace(" ", "+")
 
-    # Proxy
-    proxies = None
-    if proxy:
-        if proxy.startswith("https"):
-            proxies = {"https": proxy}
-        else:
-            proxies = {"http": proxy}
+    proxies = (
+        {"https": proxy}
+        if proxy and proxy.startswith("https")
+        else {"http": proxy}
+        if proxy
+        else None
+    )
 
-    # Fetch
     start = 0
+    results = []
     while start < num_results:
-        # Send request
-        resp = _req(escaped_term, num_results - start,
-                    lang, start, proxies, timeout)
-
-        # Parse
+        resp = _req(escaped_term, num_results - start, lang, start, proxies, timeout)
         soup = BeautifulSoup(resp.text, "html.parser")
-        result_block = soup.find_all("div", attrs={"class": "g"})
+        result_block: List[BeautifulSoup] = soup.select("div.g")
+
         for result in result_block:
-            # Find link, title, description
-            link = result.find("a", href=True)
-            title = result.find("h3")
-            description_box = result.find(
-                "div", {"style": "-webkit-line-clamp:2"})
-            if description_box:
-                description = description_box.text
-                if link and title and description:
-                    start += 1
-                    if advanced:
-                        yield SearchResult(link["href"], title.text, description)
-                    else:
-                        yield link["href"]
+            link, title, description = (
+                result.find("a", href=True),
+                result.select_one("h3"),
+                result.select_one('div[style="-webkit-line-clamp:2"]'),
+            )
+
+            if link and title and description:
+                start += 1
+                result_item = (
+                    SearchResult(link["href"], title.text, description.text)
+                    if advanced
+                    else link["href"]
+                )
+                results.append(result_item)
         sleep(sleep_interval)
 
         if start == 0:
-            return []
+            break
+    return results
